bcf3cbb7882b4267ac202e2d2791aa523e95cb9b
linus muema <linus.m.muema@gmail.com>
2020-07-10 12:56:15 +0300

    - initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+
+const  dbURI = "mongodb://localhost/pies"
+app.use(express.json())
+
+
+mongoose.connect(dbURI , {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+
+db.on("error", (err)=>{console.error(err)})
+db.once("open", () => {console.log("DB started successfully")})
+
+app.listen(2400, () => {console.log("Server started: 2400")})
