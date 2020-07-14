1cd41ecbfcbcc3f6511003276fe638523afedf5e
linus muema <linus.m.muema@gmail.com>
2020-07-14 13:57:54 +0300

    - change missing script

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const authRoute = require('./routes/auth')
 
 const  dbURI = "mongodb://localhost/pies"
 app.use(express.json())
+app.use('/', (req, res) => {res.send("Hello Auth")})
 app.use('/api/auth', authRoute)
 
 
 mongoose.connect(process.env.MONGODB_URL || dbURI , {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(res => {console.log(res.connections[0].client);})
+    .then(res => {console.log("Db started successfully");})
     .catch(err => {console.log(err);})
 const db = mongoose.connection
 
